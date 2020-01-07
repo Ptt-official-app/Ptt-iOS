76b8076c86a93a05957c77f48df854574c99df29
denkeni <denkenie@gmail.com>
2020-01-07 17:33:23 +0800

    Add initial commit

---
+++ Ptt/ViewController.swift
@@ -0,0 +1,18 @@
+//
+//  ViewController.swift
+//  Ptt
+//
+//  Created by denkeni on 2020/1/7.
+//  Copyright © 2020 Ptt. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+}
+
---
+++ PttTests/PttTests.swift
@@ -0,0 +1,34 @@
+//
+//  PttTests.swift
+//  PttTests
+//
+//  Created by denkeni on 2020/1/7.
+//  Copyright © 2020 Ptt. All rights reserved.
+//
+
+import XCTest
+@testable import Ptt
+
+class PttTests: XCTestCase {
+
+    override func setUp() {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
