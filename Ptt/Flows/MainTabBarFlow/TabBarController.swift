9cf14430465237379dd6eb0659a24701c5bf6897
賴彥宇 <jackysnsd@gmail.com>
2021-01-09 17:22:26 +0800

    [Refactor] Remove Main.storyboard, and create TabBarController rogrammatically
    - Create TabBarPage to replace Tab, this implementation you can change icons, names, order, etc very easily. Everything is in one place wrapped with enums

--- Ptt/Flows/MainTabBarFlow/TabBarController.swift
+++ Ptt/Flows/MainTabBarFlow/TabBarController.swift
@@ -8,71 +8,17 @@
 
 import UIKit
 
-private enum Tab: Int {
-    case favorite = 0
-    case hotTopic
-}
-
 final class TabBarController: UITabBarController, UITabBarControllerDelegate, TabBarView {
-    
-    // MARK: - Properties
-
-    var onViewDidLoad: ((UINavigationController) -> Void)?
-    var onFavoriteFlowSelect: ((UINavigationController) -> Void)?
-    var onHotTopicFlowSelect: ((UINavigationController) -> Void)?
     
     // MARK: - Life Cycle
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        
-        configureTabBarItems()
-        
         delegate = self
         if let controller = customizableViewControllers?.first as? UINavigationController {
             if #available(iOS 11.0, *) {
                 controller.navigationBar.prefersLargeTitles = true
             }
-            onViewDidLoad?(controller)
-        }
-    }
-    
-    // MARK: - UITabBarControllerDelegate
-    
-    func tabBarController(_ tabBarController: UITabBarController, didSelect viewController: UIViewController) {
-        guard let controller = viewControllers?[selectedIndex] as? UINavigationController,
-              let tab = Tab(rawValue: selectedIndex) else { return }
-        
-        switch tab {
-        case .favorite:
-            onFavoriteFlowSelect?(controller)
-        case .hotTopic:
-            onHotTopicFlowSelect?(controller)
-        }
-    }
-}
-
-private extension TabBarController {
-    
-    func configureTabBarItems() {
-        guard let controllers = customizableViewControllers as? [UINavigationController] else { return }
-        
-        for (index, controller) in controllers.enumerated() {
-            guard let tab = Tab(rawValue: index) else { return }
-            
-            var localizedString = ""
-            var image: UIImage = UIImage()
-            
-            switch tab {
-            case .favorite:
-                localizedString = "Favorite Boards"
-                image = StyleKit.imageOfFavoriteTabBar()
-            case .hotTopic:
-                localizedString = "Hot Topics"
-                image = StyleKit.imageOfHotTopic()
-            }
-            
-            controller.tabBarItem = UITabBarItem(title: NSLocalizedString(localizedString, comment: ""), image: image, tag: index)
         }
     }
 }
