422bcb157d213fd59f1104ea7d849824e8218f2b
marcusfu <marcussfu@gmail.com>
2021-01-29 02:21:18 +0800

    Adjust getBoardListV2 api parameters

--- Ptt/API/APIClient.swift
+++ Ptt/API/APIClient.swift
@@ -164,14 +164,14 @@ extension APIClient: APIClientProtocol {
     ///   - startIdx: starting idx, '' if fetch from the beginning.
     ///   - max: max number of the returned list, requiring <= 300
     ///   - completion: the list of board information
-    func getBoardListV2(token: String, keyword: String="", startIdx: String="", max: Int=300, completion: @escaping (BoardListResultV2) -> Void) {
+    func getBoardListV2(token: String, keyword: String="", startIdx: String="", max: Int=200, completion: @escaping (BoardListResultV2) -> Void) {
         var urlComponent = tempURLComponents
         urlComponent.path = "/api/boards"
         // Percent encoding is automatically done with RFC 3986
         urlComponent.queryItems = [
-            URLQueryItem(name: "keyword", value: keyword),
-            URLQueryItem(name: "startIdx", value: startIdx),
-            URLQueryItem(name: "max", value: "\(max)")
+            URLQueryItem(name: "title", value: keyword),
+            URLQueryItem(name: "start_idx", value: startIdx),
+            URLQueryItem(name: "limit", value: "\(max)")
         ]
         guard let url = urlComponent.url else {
             assertionFailure()
