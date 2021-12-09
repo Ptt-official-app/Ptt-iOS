09f1a9b315be46de8112afd8da1aa2fe11abe653
xxi511 <s941314g@yahoo.com.tw>
2021-12-10 01:46:16 +0800

    add unit test

--- Ptt/Extensions/StringExtension.swift
+++ Ptt/Extensions/StringExtension.swift
@@ -15,7 +15,7 @@ extension String {
             var _title = self
             let range = leftBracket...rightBracket
             _title.removeSubrange(range)
-            return _title
+            return _title.trimmingCharacters(in: .whitespacesAndNewlines)
         }
         return self
     }
@@ -23,9 +23,16 @@ extension String {
     func getBorderName() -> String {
         // Sample url
         // "http://localhost/bbs/test/M.1234567900.A.125"
+        guard self.isLink else { return "" }
         let urlSplits = self.split(separator: "/")
         guard urlSplits.count == 5 else { return "" }
         let boardName = String(urlSplits[3])
         return boardName
+    }
+
+    var isLink: Bool {
+        // Why not URL(string: )?
+        // The function said http/localhost/bbs/test/M.1234567900.A.125 is a link...
+        self.starts(with: "http://") || self.starts(with: "https://")
     }
 }
