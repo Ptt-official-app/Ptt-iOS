145b9ef532c8a0cbad5fadbf08b66bb1551b87ab
xxi511 <s941314g@yahoo.com.tw>
2021-12-18 16:30:34 +0800

    add colors

--- Ptt/Generated/PttColors.swift
+++ Ptt/Generated/PttColors.swift
@@ -16,9 +16,27 @@
 // swiftlint:disable identifier_name line_length nesting type_body_length type_name
 enum PttColors {
   static let black = ColorAsset(name: "black")
+  static let blueGrey = ColorAsset(name: "blueGrey")
+  static let brightLilac = ColorAsset(name: "brightLilac")
   static let charcoalGrey = ColorAsset(name: "charcoalGrey")
+  static let codGray = ColorAsset(name: "codGray")
+  static let coral = ColorAsset(name: "coral")
+  static let dandelion = ColorAsset(name: "dandelion")
   static let darkGrey = ColorAsset(name: "darkGrey")
+  static let darkMint = ColorAsset(name: "darkMint")
+  static let darkPeriwinkle = ColorAsset(name: "darkPeriwinkle")
+  static let deepSkyBlue = ColorAsset(name: "deepSkyBlue")
+  static let frenchGray = ColorAsset(name: "frenchGray")
+  static let lightBlueGrey = ColorAsset(name: "lightBlueGrey")
+  static let paleGrey = ColorAsset(name: "paleGrey")
+  static let paleLilac = ColorAsset(name: "paleLilac")
+  static let palePurple = ColorAsset(name: "palePurple")
+  static let reddishPink = ColorAsset(name: "reddishPink")
+  static let robinSEgg = ColorAsset(name: "robinSEgg")
+  static let shark = ColorAsset(name: "shark")
   static let slateGrey = ColorAsset(name: "slateGrey")
+  static let tangerine = ColorAsset(name: "tangerine")
+  static let tuna = ColorAsset(name: "tuna")
 }
 // swiftlint:enable identifier_name line_length nesting type_body_length type_name
 
